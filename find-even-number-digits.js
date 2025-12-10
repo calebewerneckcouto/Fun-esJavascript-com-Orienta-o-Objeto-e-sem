faf63097480ff713954cce60244cfd9633cc3e38
calebewerneckcouto <calebewerneck@hotmail.com>
2025-12-10 13:55:22 -0300

    numero par de digitos

---
+++ find-even-number-digits.js
@@ -0,0 +1,16 @@
+const findNUmbers = (nums) => {
+    let countEvenNumber = 0;
+
+    for (let value of nums) {
+        let numDigits = value.toString().length;
+
+        if (numDigits % 2 === 0) {
+            countEvenNumber++;
+        }
+    }
+
+    return countEvenNumber;  // ⬅️ fora do loop!
+}
+
+console.log(findNUmbers([12,345,2,6,7896]));     // 2
+console.log(findNUmbers([555,901,482,1771]));    // 1
